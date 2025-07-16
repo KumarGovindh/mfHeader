9618b5f99f4be42f83e9b9131a3da5b512922a85
KumarGovindh <kumar.govindh@outlook.com>
2025-07-16 14:58:13 +0530

    Create FooterButton.js

---
+++ src/FooterButton.js
@@ -0,0 +1,8 @@
+const FooterButton = () => {
+    return(
+        <button style={{ padding: '5px 10px', borderRadius: '5px', alignItems: 'center'}}>
+            Footer Button from HeaderMF
+        </button>
+    )
+}
+export default FooterButton
